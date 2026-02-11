0ee72d56b27a655da42d624e0fb2395fa43f2702
Gabriel-Dimas <garbrieldimasm@gmail.com>
2026-02-11 15:47:48 -0300

    funcionalidade do botão

---
+++ js/app.js
@@ -0,0 +1,5 @@
+function alterarStatus(id){
+   let gameEscolido = document.getElementById (`game - ${id}`); 
+   let alugado = gameEscolido.querySelector('.dashboard__item__img');
+   let botao = gameEscolido.querySelector('.dashboard__item__button');
+}
