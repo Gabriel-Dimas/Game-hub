231fbb03156400cb29074153974e2386b7c21984
Gabriel-Dimas <garbrieldimasm@gmail.com>
2026-02-12 10:34:18 -0300

    app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,22 @@
-function alterarStatus(id){
-   let gameEscolido = document.getElementById (`game - ${id}`); 
-   let alugado = gameEscolido.querySelector('.dashboard__item__img');
-   let botao = gameEscolido.querySelector('.dashboard__item__button');
-}
+function alterarStatus(id) {
+    let gameClicado = document.getElementById(`game-${id}`);
+    let escolido = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+    
+    
+        
+
+         if (escolido.classList.contains('dashboard__item__img--rented')) {
+         escolido.classList.remove('dashboard__item__img--rented');
+         botao.classList.remove('dashboard__item__button--return');
+         botao.textContent = 'Alugar';
+         } else {
+         escolido.classList.add('dashboard__item__img--rented');
+         botao.classList.add('dashboard__item__button--return');
+         botao.textContent = 'Devolver';
+      }  
+
+   
+   }
+
+
